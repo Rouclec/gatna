cd7ae07af2c9b9be39ef6efdb4c46aaa684477ea
Asonganyi Rouclec <senatorasonganyi97@gmail.com>
2024-11-03 23:55:24 +0100

    responsiveness of home page, except footer

--- src/components/CourseDetails.tsx
+++ src/components/CourseDetails.tsx
@@ -1,138 +1,218 @@
-import React, { FC } from "react";
-import Pin from "@/public/assets/icons/pin.svg";
-import { gilroyBlack, gilroyBold, gilroyRegular } from "../pages";
-import { Paper, TimeSquare, Video } from "react-iconly";
+import React, { FC } from 'react'
+import Pin from '@/public/assets/icons/pin.svg'
+import { gilroyBlack, gilroyBold, gilroyHeavy, gilroyRegular } from '../pages'
+import { Paper, TimeSquare, Video } from 'react-iconly'
 
 interface Props {
   course: {
-    videoURL: string;
-    id: string | number;
-    title: string;
-    description: string;
-    videos: string | number;
-    pdf?: string;
-    duration: string | number;
-    tag: string;
-    price: number | string;
-    watermark: string;
-  };
-  inverted?: boolean;
-  pinColor: string;
+    videoURL: string
+    id: string | number
+    title: string
+    description: string
+    videos: string | number
+    pdf?: string
+    duration: string | number
+    tag: string
+    price: number | string
+    watermark: string
+  }
+  inverted?: boolean
+  pinColor: string
 }
 
 const CourseDetails: FC<Props> = ({ course, inverted, pinColor }) => {
   return (
     <div
-      className={`h-screen flex flex-col items-center justify-center relative ${
-        inverted ? "gap-14" : "gap-24"
-      } grid grid-cols-2`}
+      // className={`flex flex-col items-center justify-center relative ${
+      //   inverted ? 'gap-14' : 'gap-24'
+      // } grid gri grid-cols-2`}
+      className={`flex flex-col md:flex-row items-center justify-self-center relative gap-7`}
+      // style={{
+      //   backgroundColor: pinColor
+      // }}
     >
-      {inverted && (
-        <div className="flex flex-col h-[445px] justify-between col-span-1">
-          <div className="flex flex-col gap-3 items-end">
-            <div className="flex bg-dark-4D p-4 w-fit items-center rounded-[10px]">
-              <Pin className="w-6 h-6" fill={pinColor} />
-              <p className={`${gilroyRegular.className} text-lg`}>
-                {course.tag} /{" "}
-                <span className={`${gilroyBold.className}`}>
-                  ${course.price}
-                </span>
-              </p>
-            </div>
+      {inverted ? (
+        // Inverted Layout: Texts - Video - Watermark
+        <div className='grid gap-4'>
+          <div className='relative flex items-center justify-center'>
             <p
-              className={`leading-[52px] text-[40px] ${gilroyBlack.className} text-right`}
+              className={`block lg:hidden ${gilroyHeavy.className} text-6xl mx:text-8xl text-center leading-none text-white text-opacity-[2%] whitespace-nowrap`}
             >
-              {course.title}
+              {course.watermark}
             </p>
-            <p className={`${gilroyRegular.className} text-lg text-right`}>
-              {course.description}
-            </p>
-            <div className="flex flex-row items-center gap-3">
-              <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                <Video size={24} style={{ opacity: 0.4 }} />
-                <p className={`${gilroyRegular.className} text-neutral-50`}>
-                  {course.videos} videos
-                </p>
-              </div>
-              {course.pdf && (
-                <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                  <Paper size={24} style={{ opacity: 0.4 }} />
-                  <p className={`${gilroyRegular.className} text-neutral-50`}>
-                    PDF
+          </div>
+          <div className='pl-0 mx:px-16 lg:px-28 flex flex-col md:flex-row gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-8 lg:gap-16'>
+              <div className='flex flex-col gap-16 col-span-1'>
+                <div className='flex flex-col gap-3 items-center md:items-end'>
+                  <div className='flex bg-dark-4D p-4 w-fit items-center rounded-[10px]'>
+                    <Pin className='w-6 h-6' fill={pinColor} />
+                    <p className={`${gilroyRegular.className} text-lg`}>
+                      {course.tag} /{' '}
+                      <span className={`${gilroyBold.className}`}>
+                        ${course.price}
+                      </span>
+                    </p>
+                  </div>
+                  <p
+                    className={`leading-[52px] text-3xl md:text-[40px] ${gilroyBlack.className} text-center md:text-right`}
+                  >
+                    {course.title}
+                  </p>
+                  <p
+                    className={`${gilroyRegular.className} text-lg text-center md:text-right`}
+                  >
+                    {course.description}
+                  </p>
+                  <div className='grid grid-cols-2 md:grid-cols-3 items-center gap-3'>
+                    {!course.pdf && <div className='hidden md:block'/>}
+                    <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                      <Video size={24} style={{ opacity: 0.4 }} />
+                      <p
+                        className={`${gilroyRegular.className} text-neutral-50`}
+                      >
+                        {course.videos} videos
+                      </p>
+                    </div>
+                    {course.pdf && (
+                      <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                        <Paper size={24} style={{ opacity: 0.4 }} />
+                        <p
+                          className={`${gilroyRegular.className} text-neutral-50`}
+                        >
+                          PDF
+                        </p>
+                      </div>
+                    )}
+                    <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                      <TimeSquare size={24} style={{ opacity: 0.4 }} />
+                      <p
+                        className={`${gilroyRegular.className} text-neutral-50`}
+                      >
+                        {course.duration} hours
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className='bg-gradient w-[254px] h-[70px] items-center self-center md:self-end justify-center flex rounded-[10px]'>
+                  <p className={`${gilroyBold.className}`}>
+                    Acheter ma formation
                   </p>
                 </div>
-              )}
-              <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                <TimeSquare size={24} style={{ opacity: 0.4 }} />
-                <p className={`${gilroyRegular.className} text-neutral-50`}>
-                  {course.duration} hours
-                </p>
+              </div>
+              <div
+                className={`w-full mx-auto h-full min-h-48 bg-neutral-24 rounded-[40px] col-span-1 items-center justify-center flex ${
+                  inverted ? 'mr-10' : 'ml-10'
+                }`}
+              >
+                <p>Video goes here</p>
               </div>
             </div>
-          </div>
-          <div className="bg-gradient w-[254px] h-[70px] items-center self-end justify-center flex rounded-[10px]">
-            <p className={`${gilroyBold.className}`}>Acheter ma formation</p>
+            <div className='relative flex items-center justify-center ml-16'>
+              <p
+                className={`hidden lg:block ${gilroyHeavy.className} text-8xl md:text-9xl leading-none text-white text-opacity-[2%] whitespace-nowrap absolute`}
+                style={{
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%) rotate(90deg)'
+                }}
+              >
+                {course.watermark}
+              </p>
+            </div>
           </div>
         </div>
-      )}
-
-      <div
-        className={`w-[526px] h-[445px] bg-neutral-24 rounded-[40px] col-span-1 items-center justify-center flex ${
-          inverted ? "mr-10" : "ml-10"
-        }`}
-      >
-        <p>Video goes here</p>
-      </div>
-      {!inverted && (
-        <div className="flex flex-col h-[445px] justify-between col-span-1">
-          <div className="flex flex-col gap-3">
-            <div className="flex bg-dark-4D p-4 w-fit items-center rounded-[10px]">
-              <Pin className="w-6 h-6" fill={pinColor} />
-              <p className={`${gilroyRegular.className} text-lg`}>
-                {course.tag} /{" "}
-                <span className={`${gilroyBold.className}`}>
-                  ${course.price}
-                </span>
+      ) : (
+        // Non-Inverted Layout: Watermark - Video - Texts
+        <div className='grid gap-4'>
+          <div className='relative flex items-center justify-center'>
+            <p
+              className={`block lg:hidden ${gilroyHeavy.className} text-6xl mx:text-8xl text-center leading-none text-white text-opacity-[2%] whitespace-nowrap`}
+            >
+              {course.watermark}
+            </p>
+          </div>
+          <div className='pl-0 mx:px-16 lg:px-28 flex flex-col md:flex-row gap-4'>
+            <div className='relative flex items-center justify-center mr-16'>
+              <p
+                className={`hidden lg:block ${gilroyHeavy.className} text-8xl md:text-9xl leading-none text-white text-opacity-[2%] whitespace-nowrap absolute`}
+                style={{
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%) rotate(90deg)'
+                }}
+              >
+                {course.watermark}
               </p>
             </div>
-            <p
-              className={`leading-[52px] text-[40px] ${gilroyBlack.className}`}
-            >
-              {course.title}
-            </p>
-            <p className={`${gilroyRegular.className} text-lg`}>
-              {course.description}
-            </p>
-            <div className="flex flex-row items-center gap-3">
-              <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                <Video size={24} style={{ opacity: 0.4 }} />
-                <p className={`${gilroyRegular.className} text-neutral-50`}>
-                  {course.videos} videos
-                </p>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-8 lg:gap-16'>
+              <div
+                className={`w-full mx-auto h-full min-h-48 bg-neutral-24 rounded-[40px] col-span-1 items-center justify-center flex mr-10`}
+              >
+                <p>Video goes here</p>
               </div>
-              {course.pdf && (
-                <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                  <Paper size={24} style={{ opacity: 0.4 }} />
-                  <p className={`${gilroyRegular.className} text-neutral-50`}>
-                    PDF
+              <div className='flex flex-col gap-16 col-span-1'>
+                <div className='flex flex-col gap-3 items-center md:items-start'>
+                  <div className='flex bg-dark-4D p-4 w-fit items-center rounded-[10px]'>
+                    <Pin className='w-6 h-6' fill={pinColor} />
+                    <p className={`${gilroyRegular.className} text-lg`}>
+                      {course.tag} /{' '}
+                      <span className={`${gilroyBold.className}`}>
+                        ${course.price}
+                      </span>
+                    </p>
+                  </div>
+                  <p
+                    className={`leading-[52px] text-3xl md:text-[40px] ${gilroyBlack.className} text-center md:text-left`}
+                  >
+                    {course.title}
+                  </p>
+                  <p
+                    className={`${gilroyRegular.className} text-lg text-center md:text-left`}
+                  >
+                    {course.description}
+                  </p>
+                  <div className='grid grid-cols-2 md:grid-cols-3 items-center gap-3'>
+                    <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                      <Video size={24} style={{ opacity: 0.4 }} />
+                      <p
+                        className={`${gilroyRegular.className} text-neutral-50`}
+                      >
+                        {course.videos} videos
+                      </p>
+                    </div>
+                    {course.pdf && (
+                      <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                        <Paper size={24} style={{ opacity: 0.4 }} />
+                        <p
+                          className={`${gilroyRegular.className} text-neutral-50`}
+                        >
+                          PDF
+                        </p>
+                      </div>
+                    )}
+                    <div className='flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3'>
+                      <TimeSquare size={24} style={{ opacity: 0.4 }} />
+                      <p
+                        className={`${gilroyRegular.className} text-neutral-50`}
+                      >
+                        {course.duration} hours
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className='bg-gradient w-[254px] h-[70px] items-center self-center md:self-start justify-center flex rounded-[10px]'>
+                  <p className={`${gilroyBold.className}`}>
+                    Acheter ma formation
                   </p>
                 </div>
-              )}
-              <div className="flex items-center justify-center rounded-lg bg-grey-bg gap-[6px] px-[14px] py-3">
-                <TimeSquare size={24} style={{ opacity: 0.4 }} />
-                <p className={`${gilroyRegular.className} text-neutral-50`}>
-                  {course.duration} hours
-                </p>
               </div>
             </div>
-          </div>
-          <div className="bg-gradient w-[254px] h-[70px] items-center justify-center flex rounded-[10px]">
-            <p className={`${gilroyBold.className}`}>Acheter ma formation</p>
           </div>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CourseDetails;
+export default CourseDetails
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,57 @@
-import Image from "next/image";
-import React, { FC } from "react";
+import Image from 'next/image'
+import React, { FC, useState } from 'react'
 // import { Filter, Home } from "react-iconly";
-import Home from "@/public/assets/icons/home.svg";
-import Profile from "@/public/assets/icons/profile.svg";
+import Home from '@/public/assets/icons/home.svg'
+import Profile from '@/public/assets/icons/profile.svg'
+import BoardMenu2 from '@/public/assets/icons/board-menu-2.svg'
 
-import logo from "@/public/assets/images/logo.png";
-import { gilroyBold, gilroyMedium } from "../pages";
-import { useRouter } from "next/router";
+import logo from '@/public/assets/images/logo.png'
+import { gilroyBold, gilroyMedium } from '../pages'
+import { useRouter } from 'next/router'
+import { CloseSquare } from 'react-iconly'
 
 // interface Props {}
 
 const Navbar: FC = () => {
-  const router = useRouter();
+  const router = useRouter()
+
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="fixed left-0 right-0 h-[104px] flex items-center gap-64 px-36 border-b-[0.5px] border-grey-bg bg-primary-500 z-50">
-      <Image src={logo} alt="logo" className="w-[170px] h-[62px] cursor-pointer" />
-      <div className="flex items-center gap-32 w-full">
-        <div className="flex items-center gap-9 w-full">
-          <div className="relative items-center bg-neutral-24 flex p-3 rounded-[14px] gap-4 w-[66px] cursor-pointer">
-            <div className="w-2 h-2 rounded-full bg-primary-400 absolute -left-3" />
-            <div className="w-4 h-4 items-center justify-center">
-              <Home className="w-4 h-4" />
+    <div className='lg:container mx-auto fixed left-0 right-0 h-[104px] flex items-center justify-between lg:justify-normal md:gap-64 px-6 mx:px-6 lg:px-36 border-b-[0.5px] border-grey-bg bg-primary-500 z-50'>
+      <Image
+        src={logo}
+        alt='logo'
+        className='w-[170px] h-[62px] cursor-pointer'
+      />
+      <div className='hidden md:flex items-center justify-between lg:justify-normal lg:gap-32 w-full'>
+        <div className='flex items-center gap-9 w-full'>
+          <div className='relative items-center bg-neutral-24 flex p-3 rounded-[14px] gap-4 w-[66px] cursor-pointer'>
+            <div className='w-2 h-2 rounded-full bg-primary-400 absolute -left-3' />
+            <div className='w-4 h-4 items-center justify-center'>
+              <Home className='w-4 h-4' />
             </div>
             <p className={`${gilroyBold.className} text-neutral-10 text-base`}>
               Home
             </p>
           </div>
-          <div className="cursor-pointer">
+          <div className='cursor-pointer'>
             <p className={`${gilroyMedium.className} text-neutral-10`}>
               About Us
             </p>
           </div>
-          <div className="cursor-pointer">
+          <div className='cursor-pointer'>
             <p className={`${gilroyMedium.className} text-neutral-10`}>
               Courses
             </p>
           </div>
         </div>
-        <div className="flex items-center gap-8 w-full">
-          <div className="flex items-center gap-2 cursor-pointer" onClick={() => router.push('/signin')}>
-            <Profile className="w-6 h-6" />
+        <div className='flex items-center justify-end lg:justify-normal gap-8 w-full'>
+          <div
+            className='flex items-center gap-2 cursor-pointer'
+            onClick={() => router.push('/signin')}
+          >
+            <Profile className='w-6 h-6' />
             {/* <p className={`${gilroyMedium.className} text-neutral-10`}>Sign in</p> */}
             <p
               className={
@@ -51,13 +62,60 @@ const Navbar: FC = () => {
               Sign in
             </p>
           </div>
-          <div className={`px-6 items-center flex justify-center py-4 rounded-[10px] bg-gradient cursor-pointer`} onClick={() => router.push('/signup')}>
-            <p className={`${gilroyBold.className} text-sm text-neutral-10`}>Create account</p>
+          <div
+            className={`px-6 items-center flex justify-center py-4 rounded-[10px] bg-gradient cursor-pointer`}
+            onClick={() => router.push('/signup')}
+          >
+            <p className={`${gilroyBold.className} text-sm text-neutral-10`}>
+              Create account
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='cursor-pointer md:hidden' onClick={() => setIsOpen(true)}>
+        <BoardMenu2 />
+      </div>
+      <div
+        className={`md:hidden bg-primary-500 shadow-lg border border-grey-bg fixed top-0 right-0 bottom-0 w-[80vw] max-w-96 z-50 transition-transform duration-300 transform ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        }`}
+      >
+        <div className='w-full h-[104px] items-center flex justify-end px-6'>
+          <div className='cursor-pointer' onClick={() => setIsOpen(false)}>
+            <CloseSquare size={40} />
+          </div>
+        </div>
+        <div className='h-full w-full flex flex-col p-6 items-end gap-6'>
+          <p className={`${gilroyBold.className} text-neutral-10 text-base cursor-pointer`}>Home</p>
+          <p className={`${gilroyBold.className} text-neutral-10 text-base cursor-pointer`}>About Us</p>
+          <p className={`${gilroyBold.className} text-neutral-10 text-base cursor-pointer`}>Courses</p>
+          <div
+            className='flex mt-10 items-center gap-2 cursor-pointer'
+            onClick={() => router.push('/signin')}
+          >
+            <Profile className="w-12 h-12" key={'profile-sidebar'}/>
+            {/* <p className={`${gilroyMedium.className} text-neutral-10`}>Sign in</p> */}
+            <p
+              className={
+                `${gilroyMedium.className} text-neutral-10 relative after:content-[''] after:block after:w-full after:h-0.5 after:bg-primary-300 after:absolute after:left-0 after:bottom-0 after:transition-all`
+                // hover:after:w-[50%]
+              }
+            >
+              Sign in
+            </p>
+          </div>
+          <div
+            className={`px-6 mt-4 items-center flex justify-center py-4 rounded-[10px] bg-gradient cursor-pointer`}
+            onClick={() => router.push('/signup')}
+          >
+            <p className={`${gilroyBold.className} text-sm text-neutral-10`}>
+              Create account
+            </p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
