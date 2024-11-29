84c3c9d4c1a60183836ca417cb1585967c1ce776
Asonganyi Rouclec <senatorasonganyi97@gmail.com>
2024-11-29 13:31:48 +0100

    setting nextauth url automatically, and adjusted the session token config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -70,12 +70,13 @@ export const authOptions: NextAuthOptions = {
   },
   cookies: {
     sessionToken: {
-      name: "next-auth.session-token",
+      name: "__Secure-next-auth.session-token",
       options: {
         httpOnly: true,
         sameSite: "lax",
         path: "/",
         secure: process.env.NODE_ENV === "production",
+        domain: process.env.NEXTAUTH_URL
       },
     },
   },
